f53eb64d7be78669dca4aca0e7176ddec914f7e5
Bindu8792 <96018616+Bindu8792@users.noreply.github.com>
2022-03-06 00:53:10 +0600

    lab files

---
+++ lab-2[PHP]/Task 2/task 2.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <title>Task 2</title>
+</head>
+
+<body>
+    
+    <form method="post" action="Check.php">
+		<fieldset>
+			<legend>Email: </legend>
+            <input type="text" name="email" value=""><br/><hr>
+            <input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+</body>
+</html>
